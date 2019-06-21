efce065afa59579cbfb4ee636226bce4237c2fb0
Lucas Borges <lucas.info@gmail.com>
2019-06-21 20:03:06 +0000

    Starting now. Initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieCard from './components/MovieCard/MovieCard';
+import 'tachyons';
+
+const App = () => {
+  return (
+
+    <MovieCard/>
+    
+  );
+}
+
+export default App;
